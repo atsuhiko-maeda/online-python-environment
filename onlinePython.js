755819dd848ca486fc8548f50966f41152440f01
atsuhiko-maeda <73572798+atsuhiko-maeda@users.noreply.github.com>
2023-10-29 20:05:16 +0900

    Add files via upload

--- onlinePython.js
+++ onlinePython.js
@@ -103,23 +103,6 @@ function setMaterial(){
         method: "GET",
       }).then(response => response.text())
       .then(text => {
-        console.log(text);
+        editor.setValue(text);
       });
 }
-
-// const sample = document.getElementById("sample");
-// const text = document.getElementById("text")
-
-// //ダイアログでファイルが選択された時
-// sample.addEventListener("change", function (event) {
-
-//   const file = event.target.files;
-//   const reader = new FileReader();
-//   reader.readAsText(file[0]);
-
-//   //読込終了後の処理
-//   reader.onload = function () {
-//     //テキストエリアに表示する
-//     text.value = reader.result;
-//   }
-// });
