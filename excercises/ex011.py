72df5e826b2270d1a95c68f21bebe10eef9573a4
atsuhiko-maeda <73572798+atsuhiko-maeda@users.noreply.github.com>
2023-12-07 23:21:13 +0900

    Update ex011.py

--- excercises/ex011.py
+++ excercises/ex011.py
@@ -3,7 +3,7 @@
 # 九九の計算を行なうプログラムを
 # 下記のコードを並べ替え，適切にインデントし，アンコメントして作成せよ．
 
-### <!---- shuffle, noindent
+### <!---- shuffle, noindent, commentout
 for i in range(1,10):
     for j in range(1,10):
         print(f"{i}x{j}={i*j}")
